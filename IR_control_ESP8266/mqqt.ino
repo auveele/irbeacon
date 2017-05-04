434add7e30b81738da1b22a919c0ad1ff39fa081
Bas116 <elbas1@hotmail.com>
2017-05-04 21:40:27 +0200

    Addición de rutinas de control
    Se ha completado el código con todos los condicionales de control para
    el manejo de periféricos. Tambien se han cambiado las rutas de los topic
    en el broker para seguir una lógica de
    BROKER/HABITACIÓN/DISPOSITIVO/PERIFERICO

--- IR_control_ESP8266/mqqt.ino
+++ IR_control_ESP8266/mqqt.ino
@@ -26,7 +26,7 @@ void setup_mqtt() {
   mqtt_base_topic = mqtt_base_topic + mqtt_client_id + "/";
   mqtt_client.setServer(MQTT_SERVER, 1883);
   mqtt_client.setCallback(mqtt_callback);
-  mqtt_client.subscribe("/IR_Beacon/#");
+  mqtt_client.subscribe("/BROKER_CASA/SALON/IR_BEACON/#");
   Serial.printf("   Server IP: %s\r\n", MQTT_SERVER);
   Serial.printf("   Username:  %s\r\n", MQTT_USER);
   Serial.println("   Cliend Id: " + mqtt_client_id);
@@ -45,7 +45,7 @@ void mqtt_reconnect() {
     // if (client.connect("ESP8266Client")) {
     if (mqtt_client.connect(mqtt_client_id.c_str(), MQTT_USER, MQTT_PASS)) {
       Serial.println("conectado");
-      mqtt_client.subscribe("/IR_Beacon/#");
+      mqtt_client.subscribe("/BROKER_CASA/SALON/IR_BEACON/#");
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqtt_client.state());
@@ -89,9 +89,89 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
  //Una vez interpretado el mensaje, pasamos la orden por IR
-  if ((mqtt_command.equals("TV_POWER"))&&(mqtt_topic.equals("/IR_Beacon/TV"))) {
+ //==========
+ //CONTROL TV
+ //==========
+  if ((mqtt_command.equals("TV_POWER"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
     TV_power(150);
     Serial.println("POWER EN TV");
   }
+  
+  if ((mqtt_command.equals("TV_VOL_UP"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_volUp(150);
+    Serial.println("VOL+ EN TV");
+  }
+  
+  if ((mqtt_command.equals("TV_VOL_DWN"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_volDwn(150);
+    Serial.println("VOL- EN TV");
+  }
+  
+  if ((mqtt_command.equals("TV_PRG_UP"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_prgUp(150);
+    Serial.println("PROG+ EN TV");
+  }
+  
+  if ((mqtt_command.equals("TV_PRG_DWN"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_prgDwn(150);
+    Serial.println("PROG- EN TV");
+  }
+  
+  if ((mqtt_command.equals("TV_MUTE"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_mute(150);
+    Serial.println("MUTE EN TV");
+  }
+  
+  if ((mqtt_command.equals("TV_INFO"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/TV"))) {
+    TV_info(150);
+    Serial.println("INFO EN TV");
+  }
 
+  //============
+  //CONTROL CHUWI
+  //============
+ if ((mqtt_command.equals("CHUWI_CLEAN"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_clean(150);
+    Serial.println("CLEAN EN CHUWI");
+  }
+
+ if ((mqtt_command.equals("CHUWI_HOME"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_home(150);
+    Serial.println("HOME EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_UP"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_up(150);
+    Serial.println("UP EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_DOWN"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_down(150);
+    Serial.println("DOWN EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_LEFT"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_left(150);
+    Serial.println("LEFT EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_RIGHT"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_right(150);
+    Serial.println("RIGHT EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_CLOCK"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_clock(150);
+    Serial.println("CLOCK EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_MAX"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_max(150);
+    Serial.println("MAX EN CHUWI");
+  }
+  
+ if ((mqtt_command.equals("CHUWI_EDGES"))&&(mqtt_topic.equals("/BROKER_CASA/SALON/IR_BEACON/CHUWI"))) {
+    chuwi_edges(150);
+    Serial.println("EDGES EN CHUWI");
+  }
 }
